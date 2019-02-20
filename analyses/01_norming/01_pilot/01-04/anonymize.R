62a3e9f44dc5f88bb0eafb64b449a2c68ff3b29a
Elisa Kreiss <mail@elisakreiss.com>
2019-02-20 13:08:37 -0800

    analyse feature norming pilot data

--- analyses/01_norming/01_pilot/01-04/anonymize.R
+++ analyses/01_norming/01_pilot/01-04/anonymize.R
@@ -1,16 +1,17 @@
 library(tidyverse)
+library(here)
 
 df_1 = read_csv(here("data","01_norming","01_pilot","01_ListFeature-norming_p1","raw.csv"))
 df_2 = read_csv(here("data","01_norming","01_pilot","02_ListFeature-norming_p2","raw.csv"))
 df_3 = read_csv(here("data","01_norming","01_pilot","03_ListFeature-norming_p3","raw.csv"))
 df_4 = read_csv(here("data","01_norming","01_pilot","04_ListFeature-norming_p4","raw.csv"))
 
-df1$pilot = "1_name3perceptualfeatures"
-df2$pilot = "2_namefirst3things"
-df3$pilot = "2_namefirst3perceptualfeatures"
-df4$pilot = "2_name3things"
+df_1$pilot = "2_3perceptualfeatures"
+df_2$pilot = "3_first3things"
+df_3$pilot = "1_first3perceptualfeatures"
+df_4$pilot = "4_3things"
 
-df = bind_rows(df_1,df_2,df_3,df4)
+df = bind_rows(df_1,df_2,df_3,df_4)
 
 anonymous_df = df %>%
   group_by(worker_id) %>% 
