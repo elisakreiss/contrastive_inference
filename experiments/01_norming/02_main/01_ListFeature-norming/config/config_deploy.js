47dda7635060bdfaef24ac02d06f6031a187f4eb
Elisa Kreiss <mail@elisakreiss.com>
2019-02-25 23:32:22 -0800

    deploymethod MTurk

--- experiments/01_norming/02_main/01_ListFeature-norming/config/config_deploy.js
+++ experiments/01_norming/02_main/01_ListFeature-norming/config/config_deploy.js
@@ -13,8 +13,8 @@ var config_deploy = {
     //'debug', 'localServer', 'MTurk', 
     // 'MTurkSandbox', 'Prolific', 'directLink'
     // "deployMethod": "debug",
-    "deployMethod": "directLink",
-    // "deployMethod": "MTurk",
+    // "deployMethod": "directLink",
+    "deployMethod": "MTurk",
 
     // who to contact in case of trouble
     "contact_email": "ekreiss@stanford.edu",
