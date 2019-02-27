d064f4747b1c8a4f0464439f5f4f4410494c892a
Elisa Kreiss <mail@elisakreiss.com>
2019-02-26 23:31:51 -0800

    add first norming data results

---
+++ analyses/01_norming/02_main/01_ListFeature-norming/anonymize.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(here)
+
+df = read_csv(here("data","01_norming","02_main","01_ListFeature-norming","raw.csv"))
+
+# df = bind_rows(df_1,df_2,df_3,df_4)
+
+anonymous_df = df %>%
+  group_by(worker_id) %>% 
+  mutate(anon_worker_id = ifelse(is.na(worker_id),
+                                 NA,
+                                 paste(sample(0:9,15,replace=TRUE),collapse = "")
+                                 )) %>% 
+  ungroup() %>% 
+  select(-worker_id,-assignment_id,-submission_id,-hit_id,-experiment_id)
+
+write.csv(anonymous_df, file = here("data","01_norming","02_main","01_ListFeature-norming","data.csv"), row.names = FALSE)
