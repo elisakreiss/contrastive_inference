4c6fcb1d071fe0a8caad8454863a03ba4e6b8283
Elisa Kreiss <mail@elisakreiss.com>
2019-03-05 00:23:33 -0800

    update config file

--- experiments/01_norming/02_main/02_ObjectNaming-norming/config/config_deploy.js
+++ experiments/01_norming/02_main/02_ObjectNaming-norming/config/config_deploy.js
@@ -4,18 +4,21 @@ var config_deploy = {
 
     // the experimentID is needed to recover data from the _babe server app
     // you receive the experimentID when you create the experiment using the _babe server app
-    "experimentID": "8",
+    "experimentID": "37",
 
     // if you use the _babe server app, specify its URL here
-    "serverAppURL": "https://babe-demo.herokuapp.com/api/submit_experiment/",
+    "serverAppURL": "https://mcmpact.ikw.uni-osnabrueck.de/babe/api/submit_experiment/",
 
     // set deployment method; use one of:
     //'debug', 'localServer', 'MTurk', 
     // 'MTurkSandbox', 'Prolific', 'directLink'
-    "deployMethod": "debug",
+    // "deployMethod": "debug",
+    // "deployMethod": "directLink",
+    // "deployMethod": "MTurk",
+    "deployMethod": "MTurkSandbox",
 
     // who to contact in case of trouble
-    "contact_email": "YOUREMAIL@wherelifeisgreat.you",
+    "contact_email": "ekreiss@stanford.edu",
 
     // OPTIONAL FIELDS
 
