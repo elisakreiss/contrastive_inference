3c722d7da25fcc47f1adcd7b6f26a90056d4c342
Elisa Kreiss <mail@elisakreiss.com>
2019-03-11 23:37:05 -0700

    create pilots for OMP norming

---
+++ experiments/01_norming/01_pilot/06_OMP-multchoice/scripts/experiment.js
@@ -0,0 +1,37 @@
+// customize the experiment by specifying a view order and a trial structure
+exp.customize = function() {
+    // record current date and time in global_data
+    this.global_data.startDate = Date();
+    this.global_data.startTime = Date.now();
+    // specify view order
+    this.views_seq = [
+        // botcaptcha,
+        // intro,
+        main,
+        postTest,
+        thanks
+    ];
+
+    // prepare information about trials (procedure)
+    // randomize main trial order, but keep practice trial order fixed
+    this.trial_info.main_trials = _.shuffle(main_trials);
+    // this.trial_info.practice_trials = practice_trials;
+
+    // preload images
+    function preloadImage(pic_stims){
+        for (pos in pic_stims){
+            (new Image()).src = "images/" + pic_stims[pos] + ".png";
+        };
+        console.log("pictures are loaded");
+    };
+    preloadImage(main_trials);
+
+    // adds progress bars to the views listed
+    // view's name is the same as object's name
+    this.progress_bar_in = ["main"];
+    // this.progress_bar_in = ['practice', 'main'];
+    // styles: chunks, separate or default
+    this.progress_bar_style = "default";
+    // the width of the progress bar or a single chunk
+    this.progress_bar_width = 100;
+};
---
+++ experiments/01_norming/01_pilot/06_OMP-multchoice/trial_info/main_trials.js
@@ -0,0 +1,24 @@
+var main_trials = [
+	//
+	// color diagnostic 
+	// typical
+	"green_broccoli",
+	"orange_carrot",
+	"white_swan",
+	"yellow_banana",
+	// atypical
+	"orange_lettuce",
+	"red_corn",
+	"white_pumpkin",
+	"yellow_egg",
+	//
+	// non-color diagnostic
+	"black_feather",
+	"blue_sponge",
+	"green_scissors",
+	"orange_butterfly",
+	"orange_mug",
+	"red_bellpepper",
+	"red_book",
+	"yellow_jacket"
+];
