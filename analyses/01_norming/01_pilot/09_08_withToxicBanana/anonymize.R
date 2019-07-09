3318db531c7441cdc232f342addbb00ee4f0e814
Elisa Kreiss <mail@elisakreiss.com>
2019-07-09 12:09:58 -0700

    initial pilot analysis

---
+++ analyses/01_norming/01_pilot/09_08_withToxicBanana/anonymize.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(here)
+
+df = read_csv(here("data","01_norming","01_pilot","09_08_withToxicBanana","raw.csv"))
+
+anonymous_df = df %>%
+  group_by(worker_id) %>% 
+  mutate(anon_worker_id = ifelse(is.na(worker_id),
+                                 NA,
+                                 paste(sample(0:9,15,replace=TRUE),collapse = "")
+  )) %>% 
+  ungroup() %>% 
+  select(-worker_id,-assignment_id,-submission_id,-hit_id,-experiment_id)
+
+write.csv(anonymous_df, file = here("data","01_norming","01_pilot","09_08_withToxicBanana","data.csv"), row.names = FALSE)
