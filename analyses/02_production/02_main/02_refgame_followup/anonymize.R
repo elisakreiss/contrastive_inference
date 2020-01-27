4aae6b92377541495449aaed6c15a35fa0670b1a
Elisa Kreiss <mail@elisakreiss.com>
2020-01-27 00:38:22 -0800

    final clean-up and figure creation for paper

---
+++ analyses/02_production/02_main/02_refgame_followup/anonymize.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(here)
+
+df = read_csv(here("data","02_production","02_main","02_refgame_followup","raw.csv"))
+
+anonymous_df = df %>%
+  group_by(worker_id) %>% 
+  mutate(anon_worker_id = ifelse(is.na(worker_id),
+                                 NA,
+                                 paste(sample(0:9,15,replace=TRUE),collapse = "")
+  )) %>% 
+  ungroup() %>% 
+  select(-worker_id,-assignment_id,-submission_id,-hit_id,-experiment_id)
+
+write.csv(anonymous_df, file = here("data","02_production","02_main","02_refgame_followup","data.csv"), row.names = FALSE)
