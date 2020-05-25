3327652e4acdb78c492ca271156bba2ce338355b
Elisa Kreiss <mail@elisakreiss.com>
2020-05-24 22:21:47 -0700

    add notes

---
+++ analyses/03_comprehension/02_main/0304_IDTpriormanipulation/anonymize.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(here)
+
+df_normal = read_csv(here("data","03_comprehension","02_main","0304_IDTpriormanipulation","raw_normal.csv"))
+df = read_csv(here("data","03_comprehension","02_main","0304_IDTpriormanipulation","raw_weird.csv")) %>% 
+  bind_rows(df_normal)
+
+anonymous_df = df %>%
+  group_by(worker_id) %>% 
+  mutate(anon_worker_id = ifelse(is.na(worker_id),
+                                 NA,
+                                 paste(sample(0:9,15,replace=TRUE),collapse = "")
+  )) %>% 
+  ungroup() %>% 
+  select(-worker_id,-assignment_id,-submission_id,-hit_id,-experiment_id)
+
+write.csv(anonymous_df, file = here("data","03_comprehension","02_main","0304_IDTpriormanipulation","data.csv"), row.names = FALSE)
